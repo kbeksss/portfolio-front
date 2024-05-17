fac218f3fcbeb84b500466669a1443179c8e24d2
kbeksss <bkadyrbekov@akhter.studio>
2024-05-18 03:54:07 +0600

    added motion to skills page

--- src/pages/Skills/index.jsx
+++ src/pages/Skills/index.jsx
@@ -1,5 +1,35 @@
 import React, { useMemo } from "react";
 import ParallaxText from "../../shared/parallax-text/index.jsx";
+import { motion } from "framer-motion";
+
+const getSliced = (array, slicedTo = 7) => {
+  let outerArr = [];
+  for (let i = 0; i < array.length; i += slicedTo) {
+    outerArr.push(array.slice(i, i + slicedTo));
+  }
+  return outerArr;
+};
+
+const Skills = () => {
+  const slicedSkills = useMemo(() => getSliced(skills, 5), []);
+  console.log(slicedSkills);
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.4 }}
+      exit={{ opacity: 0, transition: { duration: 0.4 } }}
+    >
+      <section key={"skills"}>
+        {slicedSkills.map((skillSet, index) => (
+          <ParallaxText key={index} baseVelocity={index % 2 ? -2 : 2}>
+            {skillSet.join(" ")}
+          </ParallaxText>
+        ))}
+      </section>
+    </motion.div>
+  );
+};
 
 let skills = [
   "JavaScript",
@@ -46,27 +76,5 @@ let skills = [
   "hubl",
   "hubdb",
 ];
-
-const getSliced = (array, slicedTo = 7) => {
-  let outerArr = [];
-  for (let i = 0; i < array.length; i += slicedTo) {
-    outerArr.push(array.slice(i, i + slicedTo));
-  }
-  return outerArr;
-};
-
-const Skills = () => {
-  const slicedSkills = useMemo(() => getSliced(skills, 5), []);
-  console.log(slicedSkills);
-  return (
-    <section>
-      {slicedSkills.map((skillSet, index) => (
-        <ParallaxText key={index} baseVelocity={index % 2 ? -2 : 2}>
-          {skillSet.join(" ")}
-        </ParallaxText>
-      ))}
-    </section>
-  );
-};
 
 export default Skills;
